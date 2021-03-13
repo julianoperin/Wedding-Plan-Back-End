75bbb6e253de8a6b4638258c56336c8cfc64031d
‘Juliano <julianoperins@gmail.com>
2021-03-13 14:57:13 -0500

    created the user schema

--- routes/register.js
+++ routes/register.js
@@ -5,13 +5,13 @@ router.post(
   "/",
   [
     check("name", "Please enter a namer").not().isEmpty(),
-    check("email", "Please enter a valid email").not().isEmail(),
+    check("email", "Please enter a valid email").isEmail(),
     check("password", "Please enter a 6 digit password").isLength({ min: 6 }),
   ],
   (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
+      return res.status(400).json({ errors: errors.array() });
     }
     res.send("success");
     const { name, email, password } = req.body;
